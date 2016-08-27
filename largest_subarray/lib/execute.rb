0be5b54bd729e8e5b83443ab519217be6365773a
sjain1107 <sakshijain1107@gmail.com>
2016-08-27 13:53:43 +0530

    Display result

--- largest_subarray/lib/execute.rb
+++ largest_subarray/lib/execute.rb
@@ -6,4 +6,5 @@ while !input.empty?
   input = gets.chomp.split().map { |e| e.to_i }
   csa.raw_array.push input
 end
-csa.largest_contiguous_subarray_sum
+result = csa.largest_contiguous_subarray_sum
+puts result
